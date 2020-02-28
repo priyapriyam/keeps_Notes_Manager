422f79879d490779da0629d6120f29b7f8db5c2f
priyapriyam <priyam18@navgurukul.org>
2020-02-28 13:06:19 +0530

    In this file I did CRUD oprations like we can post the data,update the data,get the data and remove the data,with this methods we used knex also.

---
+++ routes/api.js
@@ -0,0 +1,84 @@
+const knex = require('../Model/knex_queries')
+module.exports = (note, sleep) =>{
+    note.post('/add', (req,res) => {
+        let details = {                                                                                  
+            title:req.body.title,
+            notes:req.body.notes
+        }
+        knex.post_data(details)
+        .then((result )=>{
+            console.log(result)
+            res.send("data inserted")
+        })
+        .catch ((err)=>{
+            res.send (err)
+    
+        })
+    });
+
+    knex.read_data()
+    .then((result) => {
+        var empty = 0;
+        for (var i = 0; i < result.length; i++){         
+            var timer=(result[i].timer)
+            var title =result[i]['title']
+            var id = result[i].notes_id
+                // console.log(id)
+            if (timer!=null){
+                sleep(timer*60000) // 1 second = 1000,1 minute = 60000 
+                    console.log("priya see ,what you have to do in this time",title)
+                    knex.for_update_null(id)
+                    .then((result)=>{
+                    })
+                    .catch((err)=>{
+                        console.log(err)
+                    })
+            }
+            }
+
+        })
+        .catch((err)=>{
+            console.log({"message":err});
+        })
+
+
+
+    note.put('/change_data/:notes_id',(req,res) => {
+        let notes_id = req.params.notes_id;
+            let notes_details = {
+                title:req.body.title,
+                notes:req.body.notes,
+                timer:req.body.timer
+            }
+        knex.update_data(notes_details,notes_id)
+
+        .then((result) => {
+            res.send("data updated")
+            console.log("data updated")  
+        })
+        .catch((err) => {
+            res.send ("err")
+            console.log("there is error")
+        })
+        
+    })
+
+
+    note.delete('/delete_data/:notes_id',(req,res) => {
+        let notes_id = req.params.notes_id;
+        // console.log(notes_id)
+        let notes_details = {
+            title:req.body.title,
+            notes:req.body.notes
+        }
+        // console.log(notes_details)
+    knex.delete_data(notes_details,notes_id)
+        .then(() => {
+            res.send("data deleted")
+        })
+        .catch((err) => {
+            res.send(err)
+            console.log("there is error")
+        })
+    })
+}
