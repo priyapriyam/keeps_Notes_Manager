2089990b26f5857d2ddcaf0b3529b7bc67185574
priyapriyam <priyam18@navgurukul.org>
2020-02-28 12:58:33 +0530

    In this file I have wrriten knex quries for posting data,updating data,deleting data and read data.

---
+++ Model/knex_queries.js
@@ -0,0 +1,26 @@
+const knex = require('./data_base_connection')
+
+let post_data = (details) =>{
+   return knex('Important_Notes').insert(details)
+}
+
+let read_data = () =>{
+    return knex.select("*").from('Important_Notes')
+}
+
+
+let update_data =(notes_details,notes_id)=>{
+    return knex('Important_Notes').update({"title":notes_details.title,"notes":notes_details.notes,"timer":notes_details.timer})
+    .where('Important_Notes.notes_id',notes_id)
+
+}
+
+let delete_data  = (notes_details,notes_id)=>{
+    return knex('Important_Notes').del(notes_details).where("notes_id",notes_id)
+} 
+
+let for_update_null = (id)=>{
+    return  knex('Important_Notes').update({"Important_Notes.timer":null} ).where ("notes_id",id)
+}
+
+module.exports = {post_data , read_data,update_data, delete_data, for_update_null}
